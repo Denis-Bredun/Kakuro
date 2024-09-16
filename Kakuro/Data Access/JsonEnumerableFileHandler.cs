f28d65dda91a62470853df27cf927bc18c39d552
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-16 14:17:02 +0300

    feat: added default JsonEnumerableFileHandler

---
+++ Kakuro/Data Access/JsonEnumerableFileHandler.cs
@@ -0,0 +1,17 @@
+﻿using Kakuro.Interfaces.Data_Access;
+
+namespace Kakuro.Data_Access
+{
+    public class JsonEnumerableFileHandler<T> : IJsonEnumerableFileHandler<T>
+    {
+        public IEnumerable<T> Load(string filepath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IEnumerable<T> data, string filepath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
