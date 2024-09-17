44cdf189ece09cddb3f8f2c09d3013e49960f33c
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-17 23:47:32 +0300

    Added constructor to class DashboardItem

--- Kakuro/Models/DashboardItem.cs
+++ Kakuro/Models/DashboardItem.cs
@@ -2,7 +2,13 @@
 {
     public class DashboardItem
     {
+        private const int MAX_COUNT_OF_NOTES = 9;
         public int? Value { get; set; }
         public int[]? Notes { get; set; }
+
+        public DashboardItem()
+        {
+            Notes = new int[MAX_COUNT_OF_NOTES];
+        }
     }
 }
