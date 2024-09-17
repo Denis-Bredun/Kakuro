1ce9af73a1d43eecdbe77040a5c62e1721b31044
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-17 18:13:30 +0300

    Started integration tests on RatingRecordRepository and SavepointRepository

---
+++ Kakuro.Tests/Integration Tests/RatingRecordRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Kakuro.Data_Access;
+using Kakuro.Models;
+
+namespace Kakuro.Tests.Integration_Tests
+{
+    public class RatingRecordRepositoryTests
+    {
+        private const string DIRECTORY_PATH = "..\\..\\..\\Integration Tests\\Files\\RatingRecordRepositoryTests\\";
+        private JsonEnumerableFileHandler<RatingRecord> _jsonEnumerableFileHandler;
+        private RatingRecordRepository _ratingRecordRepository;
+
+        public RatingRecordRepositoryTests()
+        {
+            _jsonEnumerableFileHandler = new JsonEnumerableFileHandler<RatingRecord>();
+            _ratingRecordRepository = new RatingRecordRepository(_jsonEnumerableFileHandler);
+        }
+
+
+    }
+}
---
+++ Kakuro.Tests/Integration Tests/SavepointRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Kakuro.Data_Access;
+using Kakuro.Models;
+
+namespace Kakuro.Tests.Integration_Tests
+{
+    public class SavepointRepositoryTests
+    {
+        private const string DIRECTORY_PATH = "..\\..\\..\\Integration Tests\\Files\\SavepointRepositoryTests\\";
+        private JsonEnumerableFileHandler<Savepoint> _jsonEnumerableFileHandler;
+        private SavepointRepository _savepointRepository;
+
+        public SavepointRepositoryTests()
+        {
+            _jsonEnumerableFileHandler = new JsonEnumerableFileHandler<Savepoint>();
+            _savepointRepository = new SavepointRepository(_jsonEnumerableFileHandler);
+        }
+
+
+    }
+}
