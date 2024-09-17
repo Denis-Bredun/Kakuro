807f4bc32843f1b0fb3f775acfb30788126b912a
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-17 11:48:13 +0300

    Added default implementation of RatingRecordRepository and SavepointRepository

---
+++ Kakuro/Data Access/RatingRecordRepository.cs
@@ -0,0 +1,28 @@
+﻿using Kakuro.Enums;
+using Kakuro.Interfaces.Data_Access;
+using Kakuro.Models;
+
+namespace Kakuro.Data_Access
+{
+    public class RatingRecordRepository : IReadAllRepository<RatingRecord, DifficultyLevels>
+    {
+        private IJsonEnumerableFileHandler<RatingRecord> _jsonEnumerableFileHandler;
+        private const string DIRECTORY_PATH = "Rating Tables\\";
+        private const string PART_OF_FILEPATH = ". Rating Table.json";
+
+        public RatingRecordRepository(IJsonEnumerableFileHandler<RatingRecord> jsonEnumerableFileHandler)
+        {
+            _jsonEnumerableFileHandler = jsonEnumerableFileHandler;
+        }
+
+        public void Add(RatingRecord entity, DifficultyLevels key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RatingRecord> GetAll(DifficultyLevels key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kakuro/Data Access/SavepointRepository.cs
@@ -0,0 +1,36 @@
+﻿using Kakuro.Interfaces.Data_Access;
+using Kakuro.Models;
+
+namespace Kakuro.Data_Access
+{
+    public class SavepointRepository : IRepository<Savepoint>
+    {
+        private IJsonEnumerableFileHandler<Savepoint> _jsonEnumerableFileHandler;
+        private const string FILEPATH = "Savepoints.json";
+
+        public SavepointRepository(IJsonEnumerableFileHandler<Savepoint> jsonEnumerableFileHandler)
+        {
+            _jsonEnumerableFileHandler = jsonEnumerableFileHandler;
+        }
+
+        public void Add(Savepoint entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Savepoint GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Savepoint entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
