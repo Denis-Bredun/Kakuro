90e40da98c12ba01638974ab4d6361e9439bb1fc
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-20 01:06:36 +0300

    Added default implementation of class RatingRecordProvider.

--- Kakuro/Data Access/Data Providers/RatingRecordProvider.cs
+++ Kakuro/Data Access/Data Providers/RatingRecordProvider.cs
@@ -5,23 +5,47 @@ using Kakuro.Models;
 
 namespace Kakuro.Data_Access.Data_Providers
 {
-    public class RatingRecordProvider : IRatingRecordProvider
+    public class RatingRecordProvider : IRatingRecordProvider, IDisposable
     {
         private IReadAllRepository<RatingRecord, DifficultyLevels> _dataService;
+        private Dictionary<DifficultyLevels, IEnumerable<RatingRecord>> _cache; // temporary improvisation of cache, lmao
+                                                                                // in the future, it's planned that cache here is cleaned every 5 min.
 
         public RatingRecordProvider(IReadAllRepository<RatingRecord, DifficultyLevels> dataService)
         {
             _dataService = dataService;
+            _cache = new Dictionary<DifficultyLevels, IEnumerable<RatingRecord>>();
         }
 
+        // Too inefficient an operation. This is a temporary solution until a proper cache service is implemented.
+        // MVP, anyway.
         public void Add(RatingRecord entity, DifficultyLevels key)
         {
-            throw new NotImplementedException();
+            _dataService.Add(entity, key);
+            var newRatingRecords = _dataService.GetAll(key);
+
+            if (IsKeyInCache(key))
+                _cache.Remove(key);
+
+            _cache.Add(key, newRatingRecords);
         }
 
         public IEnumerable<RatingRecord> GetAll(DifficultyLevels key)
         {
-            throw new NotImplementedException();
+            var ratingRecords = _cache[key];
+            if (ratingRecords == null)
+            {
+                ratingRecords = _dataService.GetAll(key);
+                _cache.Add(key, ratingRecords);
+            }
+            return ratingRecords;
         }
+
+        public void Dispose()
+        {
+            _cache.Clear(); // Honestly, I'm not sure if it's needed or not. Let it be here for some time.
+        }
+
+        private bool IsKeyInCache(DifficultyLevels key) => _cache.Count(pair => pair.Key == key) != 0;
     }
 }
