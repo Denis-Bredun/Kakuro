bebfd1c18974497b64435f67387c6bc7f3ecf5e7
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-20 01:10:23 +0300

    Starting tests with RatingRecordProvider.

---
+++ Kakuro.Tests/Integration Tests/RatingRecordProviderTests.cs
@@ -0,0 +1,30 @@
+﻿using Kakuro.Data_Access.Data_Providers;
+using Kakuro.Data_Access.Repositories;
+using Kakuro.Data_Access.Tools;
+using Kakuro.Models;
+
+namespace Kakuro.Tests.Integration_Tests
+{
+    public class RatingRecordProviderTests
+    {
+        private const string DIRECTORY_PATH = "..\\..\\..\\Integration Tests\\Files\\RatingRecordProviderTests\\";
+        private JsonFileHandler<RatingRecord> _jsonFileHandler;
+        private RatingRecordRepository _ratingRecordRepository;
+        private RatingRecordProvider _ratingRecordProvider;
+
+        public RatingRecordProviderTests()
+        {
+            _jsonFileHandler = new JsonFileHandler<RatingRecord>();
+            _ratingRecordRepository = new RatingRecordRepository(_jsonFileHandler, DIRECTORY_PATH);
+            _ratingRecordProvider = new RatingRecordProvider(_ratingRecordRepository);
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(DIRECTORY_PATH))
+                Directory.Delete(DIRECTORY_PATH, true);
+        }
+
+
+    }
+}
