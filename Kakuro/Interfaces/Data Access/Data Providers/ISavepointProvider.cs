1aa535ff28b029d99b5061e8c5b7139da932f32d
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-23 01:36:34 +0300

    Added additional comments about interfaces

--- Kakuro/Interfaces/Data Access/Data Providers/ISavepointProvider.cs
+++ Kakuro/Interfaces/Data Access/Data Providers/ISavepointProvider.cs
@@ -4,7 +4,8 @@ using Kakuro.Models;
 namespace Kakuro.Interfaces.Data_Access.Data_Providers
 {
     // #BAD: the idea for creating interfaces ISavepointProvider and IRatingRecordProvider was not to have same interfaces for
-    // Repositories and Data Providers, because they are in different system layouts. I dunno if it's a good practice. 
+    // Repositories and Data Providers, because they are in different system layouts and because I didn't think that it'd be
+    // good for DI. I dunno if it's a good practice. 
     // I should remember about it
     public interface ISavepointProvider : IRepository<Savepoint> { }
 }
