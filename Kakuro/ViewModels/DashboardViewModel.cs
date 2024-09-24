23a91cf89b27166132af08fa473f4ef3ceeff1c8
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-25 02:03:07 +0300

    Added default implementation of DashboardViewModel

---
+++ Kakuro/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Kakuro.ViewModels
+{
+    public class DashboardViewModel : ViewModelBase
+    {
+        public ObservableCollection<DashboardItemViewModel> Dashboard { get; }
+
+        public DashboardViewModel()
+        {
+            Dashboard = new ObservableCollection<DashboardItemViewModel>();
+        }
+    }
+}
