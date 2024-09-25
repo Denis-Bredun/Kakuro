d686823f25ab8618007517df6714f43dd2c2bbd4
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-25 12:05:28 +0300

    Deleted wasted code

--- Kakuro/ViewModels/DashboardViewModel.cs
+++ Kakuro/ViewModels/DashboardViewModel.cs
@@ -9,13 +9,6 @@ namespace Kakuro.ViewModels
         public DashboardViewModel()
         {
             Dashboard = new ObservableCollection<DashboardItemViewModel>();
-
-            // #PRIORITY: make forming the size of collection depending on the Difficulty level
-
-            for (int i = 0; i < 36; i++)
-            {
-                Dashboard.Add(new DashboardItemViewModel(new Models.DashboardItem()));
-            }
         }
     }
 }
