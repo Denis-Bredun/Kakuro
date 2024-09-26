0b1b862f72a1c3915d3c7f8829f2aace5ebb288f
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-26 09:41:30 +0300

    Refactored RelayCommand

--- Kakuro/Base Classes/RelayCommand.cs
+++ Kakuro/Base Classes/RelayCommand.cs
@@ -2,39 +2,20 @@
 
 namespace Kakuro.Base_Classes
 {
-    public class RelayCommand : ICommand
+    public abstract class RelayCommand : ICommand
     {
-        private readonly Action<object> _execute;
-        private readonly Func<object, bool> _canExecute;
+        public event EventHandler? CanExecuteChanged;
 
-        public RelayCommand(
-          Action<object> execute,
-          Func<object, bool> canExecute = null)
+        public virtual bool CanExecute(object? parameter)
         {
-            if (execute == null)
-            {
-                throw new ArgumentNullException(nameof(execute));
-            }
-
-            _execute = execute;
-            _canExecute = canExecute;
+            return true;
         }
 
-        public event EventHandler CanExecuteChanged;
+        public abstract void Execute(object? parameter);
 
-        public bool CanExecute(object parameter)
+        protected void OnCanExecutedChanged()
         {
-            return _canExecute == null || _canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            _execute(parameter);
-        }
-
-        public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            CanExecuteChanged?.Invoke(this, new EventArgs());
         }
     }
 }
