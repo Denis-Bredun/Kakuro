130e82d17fa42b9bc8756c4565a8a00a9e3de741
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-26 09:56:13 +0300

    Added default implementations of commands for applying difficulties

---
+++ Kakuro/Commands/ApplyHardDifficultyCommand.cs
@@ -0,0 +1,12 @@
+﻿using Kakuro.Base_Classes;
+
+namespace Kakuro.Commands
+{
+    public class ApplyHardDifficultyCommand : RelayCommand
+    {
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kakuro/Commands/ApplyNormalDifficultyCommand.cs
@@ -0,0 +1,12 @@
+﻿using Kakuro.Base_Classes;
+
+namespace Kakuro.Commands
+{
+    public class ApplyNormalDifficultyCommand : RelayCommand
+    {
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
