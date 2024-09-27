f52260f1b38c97f3ff54adcfe2a731f6de1864d7
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-28 01:37:40 +0300

    Added tests for DashboardTemplateProvider

---
+++ Kakuro.Tests/Unit Tests/DashboardTemplateProviderTests.cs
@@ -0,0 +1,109 @@
+﻿using Kakuro.Data_Access.Data_Providers;
+using Kakuro.Enums;
+
+namespace Kakuro.Tests.Unit_Tests
+{
+    public class DashboardTemplateProviderTests
+    {
+        private readonly DashboardTemplateProvider _provider;
+
+        public DashboardTemplateProviderTests()
+        {
+            _provider = new DashboardTemplateProvider();
+        }
+
+        [Fact]
+        public void GenerateTemplate_ShouldReturnEasyTemplate_WhenDifficultyIsEasy()
+        {
+            // Arrange
+            var expectedTemplate = new string[,]
+            {
+                { "",   "",   "",     "",     "",     "",     "",     "" },
+                { "",   "",   "*",    "",     "",     "*",    "",     "" },
+                { "",   "*",  "*",    "*",    "*",    "*",    "*",    "" },
+                { "",   "",   "*",    "",     "",     "*",    "",     "" },
+                { "",   "*",  "*",    "*",    "*",    "*",    "*",    "" },
+                { "",   "",   "*",    "*",    "*",    "*",    "",     "" },
+                { "",   "",   "",     "*",    "*",    "",     "",     "" },
+                { "",   "",   "",     "",     "",     "",     "",     "" }
+            };
+
+            // Act
+            var result = _provider.GenerateTemplate(DifficultyLevels.Easy);
+
+            // Assert
+            Assert.Equal(expectedTemplate, result);
+        }
+
+        [Fact]
+        public void GenerateTemplate_ShouldReturnNormalTemplate_WhenDifficultyIsNormal()
+        {
+            // Arrange
+            var expectedTemplate = new string[,]
+            {
+                { "",    "",    "",    "",      "",      "",      "",      "",      "",      "",      "",       "" },
+                { "",    "*",   "",    "",      "*",     "",      "",      "*",     "",      "",      "*",      "" },
+                { "",    "*",   "*",   "",      "*",     "",      "",      "*",     "",      "*",     "*",      "" },
+                { "",    "*",   "*",   "*",     "*",     "",      "",      "*",     "*",     "*",     "*",      "" },
+                { "",    "",    "*",   "*",     "*",     "",      "",      "*",     "*",     "*",     "",       "" },
+                { "",    "*",   "",    "*",     "*",     "*",     "*",     "*",     "*",     "",      "*",      "" },
+                { "",    "*",   "*",   "",      "*",     "*",     "*",     "*",     "",      "*",     "*",      "" },
+                { "",    "*",   "",    "*",     "*",     "",      "",      "*",     "*",     "",      "*",      "" },
+                { "",    "",    "*",   "*",     "",      "",      "",      "",      "*",     "*",     "",       "" },
+                { "",    "*",   "*",   "",      "*",     "",      "",      "*",     "",      "*",     "*",      "" },
+                { "",    "*",   "",    "",      "",      "*",     "*",     "",      "",      "",      "*",      "" },
+                { "",    "",    "",    "",      "",      "",      "",      "",      "",      "",      "",       "" }
+            };
+
+            // Act
+            var result = _provider.GenerateTemplate(DifficultyLevels.Normal);
+
+            // Assert
+            Assert.Equal(expectedTemplate, result);
+        }
+
+        [Fact]
+        public void GenerateTemplate_ShouldReturnHardTemplate_WhenDifficultyIsHard()
+        {
+            // Arrange
+            var expectedTemplate = new string[,]
+            {
+                { "",    "",    "",    "",    "",       "",       "",      "",      "",      "",      "",      "",       "",       "",      "",       "",       "",    ""  },
+                { "",    "",    "",    "*",   "*",      "",       "",      "",      "",      "",      "*",     "*",      "*",      "*",     "*",      "*",      "",    ""  },
+                { "",    "",    "*",   "*",   "*",      "*",      "",      "",      "",      "*",     "*",     "*",      "",       "",      "",       "*",      "*",   ""  },
+                { "",    "",    "",    "*",   "*",      "*",      "*",     "",      "*",     "*",     "*",     "",       "*",      "*",     "*",      "",       "*",   ""  },
+                { "",    "",    "",    "",    "*",      "*",      "",      "",      "",      "*",     "",      "*",      "*",      "*",     "*",      "*",      "",    ""  },
+                { "",    "",    "",    "*",   "*",      "*",      "*",     "",      "*",     "*",     "",      "*",      "*",      "*",     "*",      "*",      "",    ""  },
+                { "",    "",    "*",   "*",   "*",      "",       "*",     "*",     "*",     "*",     "",      "*",      "*",      "*",     "*",      "*",      "",    ""  },
+                { "",    "",    "",    "*",   "*",      "",       "",      "",      "*",     "*",     "*",     "",       "*",      "*",     "*",      "",       "*",   ""  },
+                { "",    "",    "*",   "*",   "*",      "*",      "*",     "",      "",      "",      "*",     "*",      "",       "",      "",       "*",      "*",   ""  },
+                { "",    "*",   "*",   "",    "",       "",       "*",     "*",     "*",     "",      "*",     "*",      "*",      "*",     "*",      "*",      "",    ""  },
+                { "",    "*",   "",    "*",   "*",      "*",      "",      "*",     "*",     "",      "",      "",       "",       "*",     "*",      "",       "",    ""  },
+                { "",    "",    "*",   "*",   "*",      "*",      "*",     "",      "*",     "*",     "*",     "*",      "",       "*",     "*",      "*",      "",    ""  },
+                { "",    "",    "*",   "*",   "*",      "*",      "*",     "",      "*",     "*",     "",      "*",      "*",      "*",     "*",      "",       "",    ""  },
+                { "",    "",    "*",   "*",   "*",      "*",      "*",     "",      "*",     "",      "",      "",       "*",      "*",     "",       "",       "",    ""  },
+                { "",    "*",   "",    "*",   "*",      "*",      "",      "*",     "*",     "*",     "",      "*",      "*",      "*",     "*",      "",       "",    ""  },
+                { "",    "*",   "*",   "",    "",       "",       "*",     "*",     "*",     "",      "",      "",       "*",      "*",     "*",      "*",      "",    ""  },
+                { "",    "",    "*",   "*",   "*",      "*",      "*",     "*",     "",      "",      "",      "",       "",       "*",     "*",      "",       "",    ""  },
+                { "",    "",    "",    "",    "",       "",       "",      "",      "",      "",      "",      "",       "",       "",      "",       "",       "",    ""  }
+            };
+
+            // Act
+            var result = _provider.GenerateTemplate(DifficultyLevels.Hard);
+
+            // Assert
+            Assert.Equal(expectedTemplate, result);
+        }
+
+        [Fact]
+        public void GenerateTemplate_ShouldThrowNotImplementedException_WhenDifficultyIsInvalid()
+        {
+            // Arrange
+            var invalidDifficulty = (DifficultyLevels)999;
+
+            // Act & Assert
+            Assert.Throws<NotImplementedException>(() => _provider.GenerateTemplate(invalidDifficulty));
+        }
+
+    }
+}
