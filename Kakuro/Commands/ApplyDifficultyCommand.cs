3e9c950599f4d35822a461718e2ec0e91117d226
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-27 10:08:11 +0300

    Added generating template for dashboard values generating.

--- Kakuro/Commands/ApplyDifficultyCommand.cs
+++ Kakuro/Commands/ApplyDifficultyCommand.cs
@@ -29,7 +29,58 @@ namespace Kakuro.Commands
 
             _dashboard.Clear();
 
+            const int COUNT_OF_PARALLEL_SIDES = 2;
 
+            int dashboardSize = DetermineTheDashboardSize(difficultyLevel) + BORDER_ITEMS_SIZE * COUNT_OF_PARALLEL_SIDES;
+
+            var template = GenerateTemplateForDashboard(dashboardSize);
+
+            // Dashboard generating...
+        }
+
+        private int DetermineTheDashboardSize(DifficultyLevels difficultyLevel) => difficultyLevel switch
+        {
+            DifficultyLevels.Easy => EASY_LEVEL_SIZE,
+            DifficultyLevels.Normal => NORMAL_LEVEL_SIZE,
+            DifficultyLevels.Hard => HARD_LEVEL_SIZE,
+            _ => throw new NotImplementedException()
+        };
+
+        // #* - means border and MAYBE sum
+        // * - means sum
+        // v - means value
+
+        private string[,] GenerateTemplateForDashboard(int dashboardSize)
+        {
+            string[,] template = new string[dashboardSize, dashboardSize];
+            Random random = new Random();
+
+            for (int i = 0; i < dashboardSize; i++)
+                for (int j = 0; j < dashboardSize; j++)
+                    template[i, j] = DetermineValueOfTemplate(random, i, j, dashboardSize);
+
+            return template;
+        }
+
+        private string DetermineValueOfTemplate(Random random, int i, int j, int dashboardSize)
+        {
+            if (IsElementOfBorder(i, j, dashboardSize))
+                return "#*";
+            else
+            {
+                int randomChoice = random.Next(2);
+                return randomChoice == 0 ? "*" : "v";
+            }
+        }
+
+        private bool IsElementOfBorder(int i, int j, int dashboardSize)
+        {
+            return i == 0 || i == dashboardSize - 1 || j == 0 || j == dashboardSize - 1;
+        }
+
+        private void GenerateDashboard()
+        {
+            // Dashboard generating...
         }
     }
 }
