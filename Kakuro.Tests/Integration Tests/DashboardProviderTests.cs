b76ed9489784a176c781a707d196a6e7709e7a01
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-28 17:00:40 +0300

    Added fields to DashboardProviderTests

--- Kakuro.Tests/Integration Tests/DashboardProviderTests.cs
+++ Kakuro.Tests/Integration Tests/DashboardProviderTests.cs
@@ -1,14 +1,21 @@
-﻿using Kakuro.Interfaces.Data_Access.Data_Providers;
+﻿using Kakuro.Data_Access.Data_Providers;
+using Kakuro.Interfaces.Data_Access.Data_Providers;
+
+using DashboardItemCollection = System.Collections.ObjectModel.ObservableCollection<System.Collections.ObjectModel.ObservableCollection<Kakuro.ViewModels.DashboardItemViewModel>>;
 
 namespace Kakuro.Tests.Integration_Tests
 {
     public class DashboardProviderTests
     {
+        private IDashboardTemplateProvider _dashboardTemplateProvider;
+        private DashboardItemCollection _dashboardItemCollection;
         private IDashboardProvider _dashboardProvider;
 
-        public DashboardProviderTests(IDashboardProvider dashboardProvider)
+        public DashboardProviderTests()
         {
-            _dashboardProvider = dashboardProvider;
+            _dashboardItemCollection = new DashboardItemCollection();
+            _dashboardTemplateProvider = new DashboardTemplateProvider();
+            _dashboardProvider = new DashboardProvider(_dashboardTemplateProvider, _dashboardItemCollection);
         }
 
 
