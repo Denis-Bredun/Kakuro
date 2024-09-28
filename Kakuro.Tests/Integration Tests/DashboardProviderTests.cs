7b8fe1d1438afd6dfec5c10e909a5460b0d1b2ea
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-28 16:28:54 +0300

    Starting tests on DashboardProvider

---
+++ Kakuro.Tests/Integration Tests/DashboardProviderTests.cs
@@ -0,0 +1,16 @@
+﻿using Kakuro.Interfaces.Data_Access.Data_Providers;
+
+namespace Kakuro.Tests.Integration_Tests
+{
+    public class DashboardProviderTests
+    {
+        private IDashboardProvider _dashboardProvider;
+
+        public DashboardProviderTests(IDashboardProvider dashboardProvider)
+        {
+            _dashboardProvider = dashboardProvider;
+        }
+
+
+    }
+}
