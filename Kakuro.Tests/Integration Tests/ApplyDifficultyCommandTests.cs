a7d0cdbc3c9b223b50b9dbe8aeabb09f03fe676e
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-28 22:54:18 +0300

    Starting writing tests for ApplyDifficultyCommand

---
+++ Kakuro.Tests/Integration Tests/ApplyDifficultyCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using Kakuro.Base_Classes;
+using Kakuro.Commands;
+using Kakuro.Data_Access.Data_Providers;
+using Kakuro.Interfaces.Data_Access.Data_Providers;
+
+using DashboardItemCollection = System.Collections.ObjectModel.ObservableCollection<System.Collections.ObjectModel.ObservableCollection<Kakuro.ViewModels.DashboardItemViewModel>>;
+
+namespace Kakuro.Tests.Integration_Tests
+{
+    public class ApplyDifficultyCommandTests : IDisposable
+    {
+        private IDashboardTemplateProvider _dashboardTemplateProvider;
+        private DashboardItemCollection _dashboardItemCollection;
+        private IDashboardProvider _dashboardProvider;
+        private RelayCommand _applyDifficultyCommand;
+
+        public ApplyDifficultyCommandTests()
+        {
+            _dashboardItemCollection = new DashboardItemCollection();
+            _dashboardTemplateProvider = new DashboardTemplateProvider();
+            _dashboardProvider = new DashboardProvider(_dashboardTemplateProvider, _dashboardItemCollection);
+            _applyDifficultyCommand = new ApplyDifficultyCommand(_dashboardProvider);
+        }
+
+        public void Dispose()
+        {
+            _dashboardItemCollection.Clear();
+        }
+
+
+    }
+}
