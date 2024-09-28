ced6ce8e2b013d3e3f2b689b05cfe5ac38200956
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-28 23:39:12 +0300

    Attached the ApplyDifficultyCommand command to the difficulty buttons

--- Kakuro/ViewModels/DashboardViewModel.cs
+++ Kakuro/ViewModels/DashboardViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Kakuro.Base_Classes;
-
+using Kakuro.Commands;
+using Kakuro.Data_Access.Data_Providers;
+using System.Windows.Input;
 using DashboardItemCollection = System.Collections.ObjectModel.ObservableCollection<System.Collections.ObjectModel.ObservableCollection<Kakuro.ViewModels.DashboardItemViewModel>>;
 
 namespace Kakuro.ViewModels
@@ -8,11 +10,12 @@ namespace Kakuro.ViewModels
     {
         public DashboardItemCollection Dashboard { get; }
 
-
+        public ICommand ApplyDifficultyCommand { get; }
 
         public DashboardViewModel()
         {
             Dashboard = new DashboardItemCollection();
+            ApplyDifficultyCommand = new ApplyDifficultyCommand(new DashboardProvider(new DashboardTemplateProvider(), Dashboard)); // #BAD: we need DI! I already have a commend about it
         }
     }
 }
