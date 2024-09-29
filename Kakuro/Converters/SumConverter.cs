abc6909b0f47a7136a33813444d5a79b1b20535f
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-09-29 22:23:46 +0300

    Added SumConverter. Finally made sums displaying

---
+++ Kakuro/Converters/SumConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Kakuro.Converters
+{
+    public class SumConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2 || values[0] == null || values[1] == null)
+                return string.Empty;
+
+            string sumRight = values[0].ToString();
+            string sumBottom = values[1].ToString();
+
+            return $"{sumBottom}\\{sumRight}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
