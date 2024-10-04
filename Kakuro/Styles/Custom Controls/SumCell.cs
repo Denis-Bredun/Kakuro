94e07d797eaafdd5a0ca83884e3ad73ea7aaa8e9
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-04 13:09:35 +0300

    Replaced mechanism of Sum Cells output: replaced SumConverter with Custom Component

---
+++ Kakuro/Styles/Custom Controls/SumCell.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kakuro.Styles.Custom_Controls
+{
+    // #BAD: tests shall be written
+    public class SumCell : Control
+    {
+        static SumCell()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SumCell), new FrameworkPropertyMetadata(typeof(SumCell)));
+        }
+
+        public string SumRight
+        {
+            get { return (string)GetValue(SumRightProperty); }
+            set { SetValue(SumRightProperty, value); }
+        }
+
+        public static readonly DependencyProperty SumRightProperty =
+            DependencyProperty.Register("SumRight", typeof(string), typeof(SumCell), new PropertyMetadata(string.Empty));
+
+        public string SumBottom
+        {
+            get { return (string)GetValue(SumBottomProperty); }
+            set { SetValue(SumBottomProperty, value); }
+        }
+
+        public static readonly DependencyProperty SumBottomProperty =
+            DependencyProperty.Register("SumBottom", typeof(string), typeof(SumCell), new PropertyMetadata(string.Empty));
+    }
+}
