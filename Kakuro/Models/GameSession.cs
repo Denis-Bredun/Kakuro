1a073abb4c9497679b6e33db17d222038bcffbf8
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-08 12:55:25 +0300

    Added GameSession class

---
+++ Kakuro/Models/GameSession.cs
@@ -0,0 +1,20 @@
+﻿using Kakuro.Enums;
+
+namespace Kakuro.Models
+{
+    public class GameSession
+    {
+        public DifficultyLevels Difficulty { get; }
+        public string StopWatchHours { get; }
+        public string StopWatchMinutes { get; }
+        public string StopWatchSeconds { get; }
+
+        public GameSession(DifficultyLevels difficulty, string hours, string minutes, string seconds)
+        {
+            Difficulty = difficulty;
+            StopWatchHours = hours;
+            StopWatchMinutes = minutes;
+            StopWatchSeconds = seconds;
+        }
+    }
+}
