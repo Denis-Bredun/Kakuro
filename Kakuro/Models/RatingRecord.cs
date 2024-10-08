6a8d3cfdedf99eb96d4bfac23778d0916f4cdc9f
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-08 14:02:00 +0300

    FInished logic of SaveRatingRecordCommand

--- Kakuro/Models/RatingRecord.cs
+++ Kakuro/Models/RatingRecord.cs
@@ -1,14 +1,23 @@
-﻿namespace Kakuro.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Kakuro.Models
 {
     public class RatingRecord : IComparable<RatingRecord>
     {
-        public TimeOnly GameCompletionTime { get; }
-        public DateOnly GameCompletionDate { get; }
+        public TimeOnly GameCompletionTime { get; set; }
+        public DateOnly GameCompletionDate { get; set; }
 
         public RatingRecord(int hour, int minute, int second)
         {
             GameCompletionTime = new TimeOnly(hour, minute, second);
             GameCompletionDate = DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        [JsonConstructor]
+        public RatingRecord(TimeOnly gameCompletionTime, DateOnly gameCompletionDate)
+        {
+            GameCompletionTime = gameCompletionTime;
+            GameCompletionDate = gameCompletionDate;
         }
 
         public int CompareTo(RatingRecord? other)
