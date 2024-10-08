b35da80ccd4fdc07f44156871f3eba61e7431178
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-08 11:37:33 +0300

    Made basic properties for RatingTableViewModel

--- Kakuro/ViewModels/RatingTableViewModel.cs
+++ Kakuro/ViewModels/RatingTableViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using Kakuro.Interfaces.Data_Access.Data_Providers;
+using Kakuro.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Kakuro.ViewModels
 {
@@ -7,6 +10,13 @@ namespace Kakuro.ViewModels
     public class RatingTableViewModel
     {
         private IRatingRecordProvider _ratingRecordProvider;
+
+        public ObservableCollection<RatingRecord> EasyRatingRecords { get; }
+        public ObservableCollection<RatingRecord> NormalRatingRecords { get; }
+        public ObservableCollection<RatingRecord> HardRatingRecords { get; }
+
+        public ICommand LoadRatingRecordsCommand { get; }
+        public ICommand SaveRatingRecordCommand { get; }
 
         public RatingTableViewModel(IRatingRecordProvider ratingRecordProvider)
         {
