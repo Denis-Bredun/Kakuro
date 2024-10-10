fb51cc3735382d5c9f0c12e9beafd2e3f229583b
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-10 12:14:12 +0300

    Added first settings as items, added header for settings table, made file for styles of headers

--- Kakuro/ViewModels/SettingsViewModel.cs
+++ Kakuro/ViewModels/SettingsViewModel.cs
@@ -1,7 +1,18 @@
-﻿namespace Kakuro.ViewModels
+﻿using Kakuro.Models;
+using System.Collections.ObjectModel;
+
+namespace Kakuro.ViewModels
 {
     public class SettingsViewModel
     {
+        public ObservableCollection<Setting> Settings { get; set; }
 
+        public SettingsViewModel()
+        {
+            Settings = new ObservableCollection<Setting>
+            {
+                new Setting { Name = "Show correct values", IsEnabled = false }
+            };
+        }
     }
 }
