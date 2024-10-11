fb9a61bb725a82e1f56b3509b8da5346115d7282
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-12 00:54:15 +0300

    Deleted unneccesary properties from DashboardItem model

--- Kakuro/Models/DashboardItem.cs
+++ Kakuro/Models/DashboardItem.cs
@@ -7,15 +7,13 @@ namespace Kakuro.Models
         public int? DisplayValue { get; set; }
         public int? HiddenValue { get; set; } // we need this so we could show right answers using settings
         public CellType CellType { get; set; }
-        public int[,]? Notes { get; set; }
         public int? SumRight { get; set; }
         public int? SumBottom { get; set; }
 
-
         public DashboardItem()
         {
-            Notes = new int[3, 3];
         }
+
         // #BAD: i shall write tests for this model
         public override bool Equals(object? obj)
         {
@@ -25,8 +23,7 @@ namespace Kakuro.Models
                        HiddenValue == other.HiddenValue &&
                        CellType == other.CellType &&
                        SumRight == other.SumRight &&
-                       SumBottom == other.SumBottom &&
-                       Notes.Cast<int>().SequenceEqual(other.Notes.Cast<int>());
+                       SumBottom == other.SumBottom;
             }
 
             return false;
@@ -34,12 +31,8 @@ namespace Kakuro.Models
 
         public override int GetHashCode()
         {
-            // Hash code for the array cannot be generated using a simple XOR, so I use a combination of values
-            var notesHash = Notes.Cast<int>().Aggregate(0, (current, note) => current ^ note.GetHashCode());
-
             return (DisplayValue?.GetHashCode() ?? 0) ^
                    (HiddenValue?.GetHashCode() ?? 0) ^
-                   notesHash ^
                    (SumRight?.GetHashCode() ?? 0) ^
                    (SumBottom?.GetHashCode() ?? 0) ^
                    CellType.GetHashCode();
