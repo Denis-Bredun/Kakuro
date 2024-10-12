5a2e943ee3610aef7ea062dadb3fe2b91f7bf009
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-12 22:09:24 +0300

    Made locking for Submit button when showing true answers

---
+++ Kakuro/Converters/MultiOrBooleanConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Kakuro.Converters
+{
+    public class MultiOrBooleanConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isInverted = parameter is bool invert && invert;
+
+            if (values.Length == 2 &&
+                values[0] is bool isGameCompleted &&
+                values[1] is bool showCorrectAnswers)
+            {
+                bool result = isInverted ? (isGameCompleted || showCorrectAnswers) :
+                                           (!isGameCompleted && !showCorrectAnswers);
+
+                return result;
+            }
+
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
