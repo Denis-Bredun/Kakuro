a3da686ab89a4eb383d9df1b742b11d7fe2051d9
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-15 23:24:49 +0300

    Adjusted savepoints output

--- Kakuro/ViewModels/SavepointsViewModel.cs
+++ Kakuro/ViewModels/SavepointsViewModel.cs
@@ -8,9 +8,33 @@ namespace Kakuro.ViewModels
     {
         public ObservableCollection<Savepoint> Savepoints { get; }
 
+        private Savepoint _selectedSavepoint;
+        public Savepoint SelectedSavepoint
+        {
+            get => _selectedSavepoint;
+            set
+            {
+                if (_selectedSavepoint != value)
+                {
+                    _selectedSavepoint = value;
+                    OnPropertyChanged(nameof(SelectedSavepoint));
+                }
+            }
+        }
+
         public SavepointsViewModel()
         {
             Savepoints = new ObservableCollection<Savepoint>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Savepoints.Add(new Savepoint
+                {
+                    Id = i,
+                    Dashboard = new DashboardItemCollection()
+                });
+            }
         }
     }
+
 }
