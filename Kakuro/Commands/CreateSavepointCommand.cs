dd1ce4b4c3fc238a3fb0a5aaaaf648a4e44d8533
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-10-16 01:01:54 +0300

    Made CreateSavepointCommand

---
+++ Kakuro/Commands/CreateSavepointCommand.cs
@@ -0,0 +1,40 @@
+﻿using Kakuro.Base_Classes;
+using Kakuro.Models;
+using Kakuro.ViewModels;
+using Microsoft.VisualBasic;
+
+namespace Kakuro.Commands
+{
+    public class CreateSavepointCommand : RelayCommand
+    {
+        private readonly SavepointsViewModel _savepointsViewModel;
+        private readonly DashboardViewModel _dashboardViewModel;
+
+        public CreateSavepointCommand(SavepointsViewModel savepointsViewModel, DashboardViewModel dashboardViewModel)
+        {
+            _savepointsViewModel = savepointsViewModel;
+            _dashboardViewModel = dashboardViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var dashboard = _dashboardViewModel.Dashboard;
+
+            var newId = _savepointsViewModel.Savepoints.Count + 1;
+
+            string defaultName = $"Savepoint #{newId}";
+
+            string inputName = Interaction.InputBox("Enter a name for the Savepoint:", "Create Savepoint", defaultName);
+
+            if (string.IsNullOrEmpty(inputName))
+                inputName = defaultName;
+
+            var newSavepoint = new Savepoint(
+                newId,
+                inputName,
+                dashboard);
+
+            _savepointsViewModel.Savepoints.Add(new SavepointViewModel(newSavepoint));
+        }
+    }
+}
--- Kakuro/ViewModels/SavepointsViewModel.cs
+++ Kakuro/ViewModels/SavepointsViewModel.cs
@@ -1,6 +1,6 @@
-﻿using Kakuro.Base_Classes;
+﻿using Autofac;
+using Kakuro.Base_Classes;
 using Kakuro.Commands;
-using Kakuro.Models;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -29,17 +29,12 @@ namespace Kakuro.ViewModels
         public ICommand RewriteSavepointCommand { get; }
         public ICommand DeleteSavepointCommand { get; }
 
-        public SavepointsViewModel()
+        public SavepointsViewModel(ILifetimeScope scope)
         {
             Savepoints = new ObservableCollection<SavepointViewModel>();
 
-            for (int i = 1; i <= 10; i++)
-            {
-                var savepoint = new Savepoint(i, $"Savepoint #{i}", new DashboardItemCollection());
-                Savepoints.Add(new SavepointViewModel(savepoint));
-            }
-
             DeleteSavepointCommand = new DeleteSavepointCommand(this);
+            CreateSavepointCommand = new CreateSavepointCommand(this, scope.Resolve<DashboardViewModel>());
         }
     }
 
